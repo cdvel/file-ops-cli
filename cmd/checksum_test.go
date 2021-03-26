b54e661333ec9918aa305216eddf32a40fffc90a
Cesar D. Velandia <cesar@velandia.co>
2021-03-26 20:31:08 +0800

    Implement checksum test

---
+++ cmd/checksum_test.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"testing"
+)
+
+func TestGetChecksum(t *testing.T) {
+	cases := []struct {
+		filepath  string
+		algorithm string
+		expects   string
+	}{
+		{absPath("assets/myfile.txt"), "md5", "821348cb57676aff720f5c3b4a6f3c7b"},
+		{absPath("assets/myfile.txt"), "sha1", "159abd5efb0d815114e5933de86206a70bb0a8ae"},
+		{absPath("assets/myfile.txt"), "sha256", "6b5bb6a926b59c46f80b375a4bf709e343bf1dbcc819d92996b9d31cfd6d0b5c"},
+	}
+
+	for _, testcase := range cases {
+		contents, _ := ioutil.ReadFile(testcase.filepath)
+		sum := GetChecksum(contents, testcase.algorithm)
+		assert.Equal(t, sum, testcase.expects, fmt.Sprintf("Failed test with %v", testcase.filepath))
+	}
+}
